3466d6afe22addfc442da996f9f34c901c747a26
Artnalyze <artnalyze@gmail.com>
2021-01-01 17:39:48 +0700

    update form

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '~/plugin/client-only/foundation.client.js'
+    '~/plugins/client-only/foundation.client.js',
+    '~/plugins/utils.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
