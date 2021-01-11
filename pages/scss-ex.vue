f4fc9cc6eca83cd9ead04b8b6c0bb7f9ce31aa0d
Artnalyze <artnalyze@gmail.com>
2021-01-11 11:23:46 +0700

    example scss

---
+++ pages/scss-ex.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="sc-div">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo distinctio dolorem numquam dolor perferendis accusantium laboriosam animi deserunt quis ut?</p>
+    <nav>
+        <ul>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+            <li>D</li>
+        </ul>
+    </nav>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+/* Variables */
+$font-stack: sans-serif;
+$primary-color: #6f77e0;
+$font-size-p: 18px;
+
+.sc-div {
+    font: 100% $font-stack;
+    color: $primary-color;
+    font-size: $font-size-p;
+}
+
+/* Nesting */
+nav {
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+}
+</style>
