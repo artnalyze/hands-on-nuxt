98ec046c2ea18fb3a26aa892deb39a241eabf585
Artnalyze <artnalyze@gmail.com>
2021-01-21 09:46:53 +0700

    update validating route params

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -8,13 +8,28 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-    async asyncData({ params }) {
-        let { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${params.id}`)
-        return {
-            user: data
-        }
+  async asyncData({ params }) {
+    let { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${params.id}`
+    );
+    return {
+      user: data
+    };
+  },
+  validate({ params }) {
+    let test = /^\d+$/.test(params.id)
+    if(test === false) {
+      throw new Error('User not found')
     }
-}
+    return true
+  }
+
+  /*
+  async validate({params}){
+    return new Promise(....)
+  }
+  */
+};
 </script>
