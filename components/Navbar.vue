a65f2bc8450e85f796f0e955eec5ec09d1badbaf
Artnalyze <artnalyze@gmail.com>
2021-01-23 11:22:20 +0700

    create navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav>
+    <ul class="menu align-center">
+      <li><nuxt-link to="/">Home</nuxt-link></li>
+      <li><nuxt-link to="/form">Form</nuxt-link></li>
+      <li><nuxt-link to="/motion-ui">Motion UI</nuxt-link></li>
+      <li><nuxt-link to="/scss-ex">SCSS Ex</nuxt-link></li>
+      <li><nuxt-link to="/animate">Animate</nuxt-link></li>
+      <li><nuxt-link to="/swiper">Swiper</nuxt-link></li>
+      <li><nuxt-link to="/global">Global</nuxt-link></li>
+      <li><nuxt-link to="/about">About</nuxt-link></li>
+    </ul>
+    </nav>
+</template>
